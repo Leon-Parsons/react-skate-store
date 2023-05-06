13aef70236cd9c30bcc146cb3bd8c71b7f1a2424
Leon Parsons <MY_NAME@example.com>
2023-05-06 17:15:12 +1200

    Adds menu toggle state

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Logo from '../assets/skateboard-logo.png';
 import {Link} from 'react-router-dom';
 import "../styles/NavBar.css";
+import ReorderIcon from '@mui/icons-material/Reorder';
 
 const NavBar = () => {
+
+  const [showMenu, setShowMenu] = useState(false);
+  
+  const toggleNavMenu = () => {
+    setShowMenu(!showMenu);
+  }
+
   return (
     <div className="navbar">
-      <div className="logo">
+      <div className="logo" id={showMenu ? "open" : "close"}>
         <img src={Logo} alt="PicVal"/>
+        <div className="hiddenLinks">
+          <Link to="/">Home</Link>
+          <Link to="/Products">Products</Link>
+          <Link to="/AboutUs">About</Link>
+          <Link to="/ContactUs">Contact</Link>  
+        </div>
       </div>
       <div className="menu">
         <Link to="/">Home</Link>
         <Link to="/Products">Products</Link>
-        <Link to="/AboutUs">About Us</Link>
-        <Link to="/ContactUs">Contact Us</Link> 
+        <Link to="/AboutUs">About</Link>
+        <Link to="/ContactUs">Contact</Link> 
+        <button onClick={toggleNavMenu}>
+          <ReorderIcon/>
+        </button>
       </div>
     </div>
   )
