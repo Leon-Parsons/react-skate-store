e4313e00edc84551a77a66000b897e1fa7fed6f0
Leon Parsons <MY_NAME@example.com>
2023-05-06 19:19:43 +1200

    Sets up initial home page style

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import HomeBackground from '../assets/home-background.jpg';
+import "../styles/Home.css";
 
 function Home() {
   return (
-    <div>
-      Home
-    </div>
+      <div className="home">
+        <div className="headerContainer" style={{backgroundImage: `url(${HomeBackground})`}}>
+          <h1>Legally Gray Skateboards</h1>
+          <p>Custom Decks</p>
+          <Link to="/Products">
+            <button> Explore </button>
+          </Link>
+        </div>
+      </div>
   )
 }
 
