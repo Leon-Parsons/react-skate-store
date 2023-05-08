ebbe6f6368848b9f32c72e5723341d60b3c3177b
Leon Parsons <MY_NAME@example.com>
2023-05-08 13:15:31 +1200

    Adds further home page styling - image, text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import HomeBackground from '../assets/home-background.jpg';
+import HomeBackground from '../assets/home-background2.jpg';
 import "../styles/Home.css";
 
 function Home() {
   return (
-      <div className="home">
-        <div className="headerContainer" style={{backgroundImage: `url(${HomeBackground})`}}>
+      <div className="home" style={{backgroundImage: `url(${HomeBackground})`}}>
+        <div className="headerContainer" >
           <h1>Legally Gray Skateboards</h1>
           <p>Custom Decks</p>
           <Link to="/Products">
